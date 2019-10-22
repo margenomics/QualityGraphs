09ef7f8ec6794852f3aebe9127c6e620c4d00322
machalen <marnal@imim.es>
2019-10-22 18:52:21 +0200

    New modification to the Heat.make function. Now the size of row labels is set automatically and you can choose if you want to display the column colors and the clustering algorithms to order columns and rows

--- R/Heat.make.R
+++ R/Heat.make.R
@@ -1,49 +1,99 @@
-Heat.make <- function(data.clus, cols.s, filename, filedir, rownames=TRUE, rownames.vect=NULL, cex.col=0.8, cex.row=0.3, toPNG=FALSE) {
-  #data.clus: matrix to plot in the heatmap
-  #cols.s: vector of colors corresponding to the columns in the matrix data.clus 
-  #filename: Name of the output file in .pdf format
-  #filedir: Name of the resultsDir
-  #rownames: If TRUE row names are shown in the heatmap
-  #rownames.vect: Vector with row names to be shown in the heatmap
-  #cex.col: size of colnames
-  #cex.row: size of rownames, if rownames=TRUE
-  #toPNG: If TRUE heatmap is generated in png format
+Heat.make <- function(data.clus, cols.s=NULL, filename, filedir,
+                        rownames.vect="", cex.col=0.8, clustinrows=T,
+                        clustincols=T, toPNG=FALSE) {
+  #data.clus: Numeric matrix with the intensity values to plot
+  #cols.s: Vector with colors corresponding to the conditions in the heatmap
+  #filename: name of the output file
+  #filedir: name of the resultsdir
+  #rownames.vect: vector with the names of the genes in the heatmap. If rownames.vect="" no rownames are shown
+  #cex.col: size of the column labels
+  #clustinrows:TRUE if we want to order rows based on hierarchical clustering average+correlation distances
+  #clustincols: TRUE if we want to order columns based on hierarchical clustering Ward D2 + euclidean distances
+  #toPNG: TRUE if we want .png output, FALSE if we want .pdf output
+  
+  
   require(gplots)
-  
   heatcol<-colorRampPalette(c("blue", "white","red"), space = "rgb")
-  #Dendrograma de les columnes
-  clust.cols <- hclust(dist(t(data.clus)),method="ward.D2")
-  #Dendrograma de les rows
-  clust.rows <- hclust(as.dist(1-cor(t(data.clus))),method="average")
-  if(rownames) {
-    #Fem el plot
-    if (toPNG) {
-       parameters <- setparam(colnames(data.clus))
-       png(file=file.path(filedir, paste(filename,"png", sep=".")),width=parameters$wid,height=parameters$hei,res=parameters$res)
-    } else {
-       pdf(file=file.path(filedir, paste(filename,"pdf", sep=".")))
-    }
-    heatm<-heatmap.2(as.matrix(data.clus), col = heatcol(256),
-                     dendrogram="column", Colv=as.dendrogram(clust.cols),
-                     Rowv=as.dendrogram(clust.rows),labRow=rownames.vect,
-                     scale="row",cexRow=cex.row, cexCol=cex.col, ColSideColors= cols.s,
-                     main="",key=TRUE,keysize=1,density.info="none",trace="none")
-    dev.off()
+  
+  ##################################################################################
+  #In case you want to set the colors
+  # library(RColorBrewer)
+  # cond <- gsub("_.*","",colnames(num.mtrx), perl=T)
+  # pal <- brewer.pal(length(unique(cond)),name="Dark2")
+  # map <- setNames(pal,unique(cond))
+  # colors <- map[cond]
+  ##################################################################################
+  #Set dendrogram for rows
+  if (clustinrows){
+    clust.rows <- as.dendrogram(hclust(as.dist(1-cor(t(data.clus))),method="average"))
   } else {
-    #Fem el plot
-    if (toPNG) {
-       parameters <- setparam(colnames(data.clus))
-       png(file=file.path(filedir, paste(filename,"png", sep=".")),width=parameters$wid,height=parameters$hei,res=parameters$res)
-    } else {
-       pdf(file=file.path(filedir, paste(filename,"pdf", sep=".")))
-    }
-    heatm<-heatmap.2(as.matrix(data.clus), col = heatcol(256),
-                     dendrogram="column", Colv=as.dendrogram(clust.cols),
-                     Rowv=as.dendrogram(clust.rows),labRow="",
-                     scale="row",cexRow=cex.row, cexCol=cex.col, ColSideColors= cols.s,
-                     main="",key=TRUE,keysize=1,density.info="none",trace="none")
-    dev.off()
+    clust.rows <- F
   }
+  #################################################################################
+  #Set the cex values for rows
+  if (length(rownames.vect) == 1) {
+    cex.row <- 0.1
+  } else if (length(rownames.vect) < 40) {
+    cex.row <- 0.8
+  } else if (length(rownames.vect) < 70) {
+    cex.row <- 0.5
+  } else if (length(rownames.vect) < 90) {
+    cex.row <- 0.4
+  } else if (length(rownames.vect) < 120) {
+    cex.row <- 0.3
+  } else if (length(rownames.vect) < 190) {
+    cex.row <- 0.2
+  } else {
+    warning(paste(length(rownames.vect),"rows are too many rows in the heatmap for cex.row to be defined. Maximum is 190 rows"), call. = FALSE)
+    cex.row <- 0.1
+  }
+  ################################################################################
+  if (toPNG) {
+    parameters <- setparam(colnames(data.clus))
+    png(file=file.path(filedir, paste(filename,"png", sep=".")),width=parameters$wid,height=parameters$hei,res=parameters$res)
+  } else {
+    pdf(file=file.path(filedir, paste(filename,"pdf", sep=".")))
+  }
+  ################################################################################
   
-  
+  if(!is.null(cols.s)) {
+    if(clustincols) {
+      #Dendrograma de les columnes
+      clust.cols <- as.dendrogram(hclust(dist(t(data.clus)),method="ward.D2"))
+      #Fem el plot
+      heatm<-heatmap.2(as.matrix(data.clus), col = heatcol(256),
+                       dendrogram="column", Colv=clust.cols,
+                       Rowv=clust.rows,labRow=rownames.vect,
+                       scale="row",cexRow=cex.row, cexCol=cex.col, ColSideColors= cols.s,
+                       main="",key=TRUE,keysize=1,density.info="none",trace="none")
+    } else {
+      #Fem el plot
+      heatm<-heatmap.2(as.matrix(data.clus), col = heatcol(256),
+                       dendrogram="none", Colv=F,
+                       Rowv=clust.rows,labRow=rownames.vect,
+                       scale="row",cexRow=cex.row, cexCol=cex.col, ColSideColors= cols.s,
+                       main="",key=TRUE,keysize=1,density.info="none",trace="none")
+    }
+    
+  } else {
+    
+    if(clustincols) {
+      #Dendrograma de les columnes
+      clust.cols <- as.dendrogram(hclust(dist(t(data.clus)),method="ward.D2"))
+      #Fem el plot
+      heatm<-heatmap.2(as.matrix(data.clus), col = heatcol(256),
+                       dendrogram="column", Colv=clust.cols,
+                       Rowv=clust.rows,labRow=rownames.vect,
+                       scale="row",cexRow=cex.row, cexCol=cex.col,
+                       main="",key=TRUE,keysize=1,density.info="none",trace="none")
+    } else {
+      #Fem el plot
+      heatm<-heatmap.2(as.matrix(data.clus), col = heatcol(256),
+                       dendrogram="none", Colv=F,
+                       Rowv=clust.rows,labRow=rownames.vect,
+                       scale="row",cexRow=cex.row, cexCol=cex.col,
+                       main="",key=TRUE,keysize=1,density.info="none",trace="none")
+    }
+  }
+  dev.off()
 }
